f8dcb39ee57f16c9815ded0c59dabf303736ac06
Konstantin Käfer <github@kkaefer.com>
2011-09-24 20:05:30 +0200

    initial commit

---
+++ 01_bare_minimum/modulename.cc
@@ -0,0 +1,12 @@
+#include <node/v8.h>
+#include <node/node.h>
+
+void RegisterModule(v8::Handle<v8::Object> target) {
+    // You can add properties to the module in this function. It is called
+    // when the module is required by node.
+}
+
+// Register the module with node. Note that "modulename" must be the same as
+// the basename of the resulting .node file. You can specify that name in
+// wscript. When you change it there, change it here too.
+NODE_MODULE(modulename, RegisterModule);
---
+++ README.md
@@ -0,0 +1,3 @@
+# How to write node.js modules using C++
+
+This is a tutorial for writing [node.js](http://nodejs.org/) modules in C++. It is the base and reference for my talk on writing node.js modules with V8 at [JSConf 2011 in Berlin](http://jsconf.eu/2011).
